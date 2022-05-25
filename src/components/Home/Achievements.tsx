5fc61b5ac8fc977c6a1295c2d26278b0b5d19791
Josh Daniel <joshxfi.dev@gmail.com>
2022-05-25 20:34:30 +0800

    feat: add achievements

--- src/components/Home/Achievements.tsx
+++ src/components/Home/Achievements.tsx
@@ -4,6 +4,12 @@ export const Achievements = () => {
   return (
     <section>
       <h1 className="text-4xl font-bold mt-20">My Achievements</h1>
+      <ul className="mt-4 ml-4 text-secondary list-disc">
+        <li>1st Place Track & Field Sprint</li>
+        <li>Honor Award (Grade 7-8)</li>
+        <li>High Honor Award (Grade 9-10)</li>
+        <li>Highest Honor Award (Grade 11)</li>
+      </ul>
     </section>
   );
 };
