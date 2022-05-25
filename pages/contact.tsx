699a4fe2c2cc726db594f7cd06e8298eb336fcc2
Josh Daniel <joshxfi.dev@gmail.com>
2022-05-25 22:46:42 +0800

    feat: handle submit on contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 import { IoMdSend } from "react-icons/io";
 import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <section className="flex justify-between">
       <div>
@@ -30,7 +35,20 @@ const Contact = () => {
         </div>
       </div>
 
-      <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+            toast.error("Invalid email address");
+            return;
+          }
+
+          setName("");
+          setEmail("");
+          setMessage("");
+          toast.success("Message sent!");
+        }}
+      >
         <div className="flex bg-two h-12 rounded-t items-center space-x-2 pl-4">
           {["bg-red-500", "bg-amber-500", "bg-green-500"].map((color) => (
             <i key={color} className={`${color} rounded-full p-[6px]`} />
@@ -40,26 +58,46 @@ const Contact = () => {
         <div className="w-[350px] bg-[#F9FAFB] pt-8 px-8 pb-4 rounded-b font-medium text-primary space-y-8">
           <div>
             <p className="mb-2">Name</p>
-            <input type="text" className="input" />
+            <input
+              type="text"
+              className="input"
+              placeholder="(optional)"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
 
           <div>
             <p className="mb-2">Email</p>
-            <input type="text" className="input" />
+            <input
+              required
+              type="text"
+              className="input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
 
           <div>
             <p className="mb-2">Message</p>
-            <textarea className="input min-h-[80px] max-h-[200px]" />
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+              className="input min-h-[80px] max-h-[200px]"
+            />
           </div>
 
           <div className="flex justify-end">
-            <button className="bg-two px-4 py-2 hover:bg-primary transition-colors text-white flex space-x-3 rounded font-medium items-center mt-4">
+            <button
+              type="submit"
+              className="bg-two px-4 py-2 hover:bg-primary transition-colors text-white flex space-x-3 rounded font-medium items-center mt-4"
+            >
               <p>Send</p> <IoMdSend />
             </button>
           </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
